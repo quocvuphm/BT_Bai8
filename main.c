bd5a4138de7df1915b8fd4a1807e7169bb361e4b
quocvuphm <vupham7320@gmail.com>
2023-09-07 10:19:46 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Queue {
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
+
+struct Queue* createQueue(unsigned capacity) {
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = capacity - 1;  // rear ban đầu ở cuối hàng đợi trước
+    queue->array = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
+}
+
+int isFull(struct Queue* queue) {
+    return (queue->size == queue->capacity);
+}
+
+int isEmpty(struct Queue* queue) {
+    return (queue->size == 0);
+}
+
+void enqueue(struct Queue* queue, int item) {
+    if (isFull(queue)) {
+        printf("Queue is full. Cannot enqueue %d\n", item);
+        return;
+    }
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size++;
+    printf("%d enqueued to queue\n", item);
+}
+
+int dequeue(struct Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("Queue is empty. Cannot dequeue\n");
+        return -1;
+    }
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    return item;
+}
+
+int main() {
+    struct Queue* queue = createQueue(5);
+
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+    enqueue(queue, 40);
+
+    printf("%d dequeued from queue\n", dequeue(queue));
+    printf("%d dequeued from queue\n", dequeue(queue));
+
+    enqueue(queue, 50);
+
+    printf("Front item is %d\n", queue->array[queue->front]);
+    printf("Rear item is %d\n", queue->array[queue->rear]);
+
+    return 0;
+}
